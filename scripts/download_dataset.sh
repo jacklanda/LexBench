7bdf776e9c748ab1bb4004fffa60a4fecf08c7eb
Yang <yangliu.real@gmail.com>
2024-01-09 17:39:59 +0800

    Update script `download_dataset.sh` [idiom done].

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -7,14 +7,21 @@ asilm_idiom_dataset_url="https://raw.githubusercontent.com/H-TayyarMadabushi/ASt
 
 id10m_dataset_url="https://raw.githubusercontent.com/Babelscape/ID10M/master/resources/bio_format/english/dev_english.tsv"
 
+# Add help for dataset downloading
+if [ "$1" == "" ] || [ "$1" == "-h" ]; then
+    echo "Usage: ./download_dataset.sh [dataset_name]"
+    echo -e "Available dataset_name: [asilm, id10m, pie]"
+    exit 0
+fi
+
 # Leverage the first arg to select the dataset
 
 # ASILM task1 for [Idiom Detection]
 if [ "$1" == "asilm" ]; then
     echo "[Downloading] ASILM task1 dataset"
     mkdir -p "dataset/idiom_detection/"
-    wget -P "dataset/idiom_detection/" -O "reference_data.csv" $asilm_ref_dataset_url
-    wget -P "dataset/idiom_detection/" -O "idiom_data.csv" $asilm_idiom_dataset_url
+    wget -O "dataset/idiom_detection/reference_data.csv" $asilm_ref_dataset_url
+    wget -O "dataset/idiom_detection/idiom_data.csv" $asilm_idiom_dataset_url
     exit 0
 fi
 
@@ -30,6 +37,6 @@ fi
 if [ "$1" == "pie" ]; then
     echo "[Downloading] PIE dataset"
     mkdir -p "dataset/idiom_paraphrase/"
-    wget -P "dataset/idiom_paraphrase/" $pie_corpus_url
+    wget -P "dataset/idiom_paraphrase/" $pie_dataset_url
     exit 0
 fi
