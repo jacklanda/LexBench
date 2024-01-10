a85ed74c40ee25cadbf7a6e48910fb2e0cf5d837
Yang <yangliu.real@gmail.com>
2024-01-10 11:19:01 +0800

    Update script `data.py` and `download_dataset.sh`.

--- scripts/data.py
+++ scripts/data.py
@@ -9,6 +9,7 @@
 
 import os
 import random
+import pandas as pd
 from pprint import pprint
 from typing import List, Optional
 
@@ -123,10 +124,92 @@ def prepare_data_idiom_extraction(
 
 
 def prepare_data_idiom_detection(
-    data_path="",
-    dump_data_path="dataset/idiom_detection/prepared/idiom_detection_prepared.tsv",
+    refer_data_path: str,
+    idiom_data_path: str,
+    dump_data_path: str = "dataset/idiom_detection/prepared/idiom_detection_prepared.tsv",
+    dedup_idiom: bool = True,
+    verbose: bool = True,
 ) -> List[str]:
-    pass
+    """
+    This function is to prepare the idiom data for evaluation.
+
+    Args:
+    - refer_data_path: the path of the refer data file
+    - idiom_data_path: the path of the idiom data file
+    - dump_data_path: the path of the prepared data file
+    """
+
+    data_examples = []
+
+    # read the data
+    refer_df = pd.read_csv(refer_data_path)
+    idiom_df = pd.read_csv(idiom_data_path)
+
+    # get a dict include the column "sentence1" and "sentence2"
+    refer_dict = {
+        item["sentence1"]: item["sentence2"]
+        for item in refer_df.to_dict(orient="records")
+    }
+
+    idiom_df["idiom"] = idiom_df["sentence1"].apply(lambda x: refer_dict.get(x, ""))
+
+    # filter all rows that have the 3 times repeats value of "sentence1" in `idiom_df`
+    sent2freq = dict()
+    for item in idiom_df["sentence1"].tolist():
+        sent2freq[item] = sent2freq.get(item, 0) + 1
+    idiom_df["frequency"] = idiom_df["sentence1"].apply(lambda x: sent2freq[x])
+
+    # get the row of frequency == 4
+    idiom_df_four = idiom_df[idiom_df["frequency"] == 4]
+
+    # iterate each 4 rows as a group in `idiom_df_four`
+    for i in range(0, len(idiom_df_four), 4):
+        # assign A, B, C, D to each row in this group
+        data_example = {
+            "id": str(i // 4).zfill(3),
+            "context": idiom_df_four.iloc[i, 1].replace("\t", " "),
+            "idiom": idiom_df_four.iloc[i, 3].replace("\t", " "),
+        }
+        letter_mapping = {0: "A", 1: "B", 2: "C", 3: "D"}
+        for j in range(4):
+            letter = letter_mapping[j]
+            data_example[letter] = idiom_df_four.iloc[i + j, 2]
+
+            if idiom_df_four.iloc[i + j, 0] == "1":
+                data_example["target"] = letter
+
+        data_examples.append(data_example)
+
+    # deduplicate `data_example` by field "idiom" for each item
+    if dedup_idiom:
+        idx = 0
+        dedup_data_examples = []
+        dedup_idiom_set = set()
+        for item in data_examples:
+            dedup_idiom_set.add(item["idiom"] + item["target"])
+        for item in data_examples:
+            if item["idiom"] + item["target"] in dedup_idiom_set:
+                item["id"] = str(idx).zfill(3)
+                dedup_data_examples.append(item)
+                dedup_idiom_set.remove(item["idiom"] + item["target"])
+                idx += 1
+        data_examples = dedup_data_examples
+
+    if dump_data_path:
+        if not os.path.exists(os.path.dirname(dump_data_path)):
+            os.makedirs(os.path.dirname(dump_data_path))
+        with open(dump_data_path, "w") as f:
+            f.write(f"id\tcontext\tidiom\tA\tB\tC\tD\ttarget\n")
+            for item in data_examples:
+                f.write(
+                    f"{item['id']}\t{item['context']}\t{item['idiom']}\t{item['A']}\t{item['B']}\t{item['C']}\t{item['D']}\t{item['target']}\n"
+                )
+
+    if verbose:
+        # rich.print_json(data=data_examples)
+        print("Total instances:", len(data_examples))
+
+    return data_examples
 
 
 if __name__ == "__main__":
@@ -137,4 +220,7 @@ if __name__ == "__main__":
     # print(len(idiom_extraction_examples))
     # pprint(data=idiom_list)
     # rich.print_json(data=idiom_list)
-    pass
+    idiom_detection_examples = prepare_data_idiom_detection(
+        refer_data_path="/Users/jacklanda/Desktop/LexBench/scripts/dataset/idiom_detection/reference_data.csv",
+        idiom_data_path="/Users/jacklanda/Desktop/LexBench/scripts/dataset/idiom_detection/idiom_data.csv",
+    )
--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -2,8 +2,12 @@
 
 pie_dataset_url="https://raw.githubusercontent.com/zhjjn/MWE_PIE/main/data_cleaned.csv"
 
-asilm_ref_dataset_url="https://raw.githubusercontent.com/H-TayyarMadabushi/AStitchInLanguageModels/main/Dataset/Task1/SubTaskA/EN/ContextIncluded_IdiomIncluded/test.csv"
-asilm_idiom_dataset_url="https://raw.githubusercontent.com/H-TayyarMadabushi/AStitchInLanguageModels/main/Dataset/Task1/SubTaskB/EN/ContextIncluded/test.csv"
+asilm_ref_train_dataset_url="https://raw.githubusercontent.com/H-TayyarMadabushi/AStitchInLanguageModels/main/Dataset/Task1/SubTaskA/EN/ContextIncluded_IdiomIncluded/train_zero_shot.csv"
+asilm_idiom_train_dataset_url="https://raw.githubusercontent.com/H-TayyarMadabushi/AStitchInLanguageModels/main/Dataset/Task1/SubTaskB/EN/ContextIncluded/train_zero_shot.csv"
+asilm_ref_dev_dataset_url="https://raw.githubusercontent.com/H-TayyarMadabushi/AStitchInLanguageModels/main/Dataset/Task1/SubTaskA/EN/ContextIncluded_IdiomIncluded/dev.csv"
+asilm_idiom_dev_dataset_url="https://raw.githubusercontent.com/H-TayyarMadabushi/AStitchInLanguageModels/main/Dataset/Task1/SubTaskB/EN/ContextIncluded/dev.csv"
+asilm_ref_test_dataset_url="https://raw.githubusercontent.com/H-TayyarMadabushi/AStitchInLanguageModels/main/Dataset/Task1/SubTaskA/EN/ContextIncluded_IdiomIncluded/test.csv"
+asilm_idiom_test_dataset_url="https://raw.githubusercontent.com/H-TayyarMadabushi/AStitchInLanguageModels/main/Dataset/Task1/SubTaskB/EN/ContextIncluded/test.csv"
 
 id10m_dataset_url="https://raw.githubusercontent.com/Babelscape/ID10M/master/resources/bio_format/english/dev_english.tsv"
 
@@ -20,8 +24,16 @@ fi
 if [ "$1" == "asilm" ]; then
     echo "[Downloading] ASILM task1 dataset"
     mkdir -p "dataset/idiom_detection/"
-    wget -O "dataset/idiom_detection/reference_data.csv" $asilm_ref_dataset_url
-    wget -O "dataset/idiom_detection/idiom_data.csv" $asilm_idiom_dataset_url
+    wget -O "dataset/idiom_detection/reference_train_data.csv" $asilm_ref_train_dataset_url
+    wget -O "dataset/idiom_detection/idiom_train_data.csv" $asilm_idiom_train_dataset_url
+    wget -O "dataset/idiom_detection/reference_dev_data.csv" $asilm_ref_dev_dataset_url
+    wget -O "dataset/idiom_detection/idiom_dev_data.csv" $asilm_idiom_dev_dataset_url
+    wget -O "dataset/idiom_detection/reference_test_data.csv" $asilm_ref_test_dataset_url
+    wget -O "dataset/idiom_detection/idiom_test_data.csv" $asilm_idiom_test_dataset_url
+
+    cat "dataset/idiom_detection/reference_train_data.csv" "dataset/idiom_detection/reference_dev_data.csv" "dataset/idiom_detection/reference_test_data.csv" > "dataset/idiom_detection/reference_data.csv"
+    cat "dataset/idiom_detection/idiom_train_data.csv" "dataset/idiom_detection/idiom_dev_data.csv" "dataset/idiom_detection/idiom_test_data.csv" > "dataset/idiom_detection/idiom_data.csv"
+    rm -rf "dataset/idiom_detection/reference_train_data.csv" "dataset/idiom_detection/idiom_train_data.csv" "dataset/idiom_detection/reference_dev_data.csv" "dataset/idiom_detection/idiom_dev_data.csv" "dataset/idiom_detection/reference_test_data.csv"  "dataset/idiom_detection/idiom_test_data.csv"
     exit 0
 fi
 
